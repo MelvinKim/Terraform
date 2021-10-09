ca5d401136f883e31efbbf1a4b62b3303b9637cd
Melvin Kimathi <53833444+MelvinKim@users.noreply.github.com>
2021-10-09 18:00:55 +0300

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default = "10.0.10.0/24"
+}
+variable avail_zone {
+    default = "eu-west-3a"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "196.207.138.244/32"
+}
+variable jenkins_ip {
+    default = "167.71.40.196/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
+variable region {
+    default = "eu-west-3"
+}
